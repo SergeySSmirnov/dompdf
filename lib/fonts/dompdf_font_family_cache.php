7ff1e80e42c248ab32e92a8c52638787c684865f
Sergey S. Smirnov <SergeySSmirnov@mail.ru>
2015-10-06 15:15:40 +0300

    Обновлены шрифты.
    Signed-off-by: Sergey S. Smirnov <SergeySSmirnov@mail.ru>

---
+++ lib/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,163 @@
+<?php return array (
+  'arial' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Arial',
+    'bold' => DOMPDF_FONT_DIR . 'Arial_Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Arial_Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Arial_Bold_Italic',
+  ),
+  'courier' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Courier_New',
+    'bold' => DOMPDF_FONT_DIR . 'Courier_New_Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Courier_New_Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Courier_New_Bold_Italic',
+  ),
+  'monospace' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Courier_New',
+    'bold' => DOMPDF_FONT_DIR . 'Courier_New_Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Courier_New_Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Courier_New_Bold_Italic',
+  ),
+  'dejavusans' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSans-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSans-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSans-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSans',
+  ),
+  'dejavu sans' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSans-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSans-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSans-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSans',
+  ),
+  'dejavu sans light' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSans-ExtraLight',
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSans-ExtraLight',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSans-ExtraLight',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSans-ExtraLight',
+  ),
+  'dejavusanscondensed' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed',
+  ),
+  'dejavu sans condensed' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed',
+  ),
+  'dejavusansmono' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSansMono-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSansMono-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSansMono-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSansMono',
+  ),
+  'dejavu sans mono' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSansMono-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSansMono-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSansMono-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSansMono',
+  ),
+  'dejavuserif' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSerif-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSerif-BoldItalic',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSerif-Italic',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSerif',
+  ),
+  'dejavu serif' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSerif-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSerif-BoldItalic',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSerif-Italic',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSerif',
+  ),
+  'dejavuserifcondensed' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-BoldItalic',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-Italic',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed',
+  ),
+  'dejavu serif condensed' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-BoldItalic',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-Italic',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed',
+  ),  
+  'times' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Times_New_Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times_New_Roman_Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times_New_Roman_Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times_New_Roman_Bold_Italic',
+  ),
+  'times-roman' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Times_New_Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times_New_Roman_Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times_New_Roman_Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times_New_Roman_Bold_Italic',
+  ),
+  'times new roman' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Times_New_Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times_New_Roman_Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times_New_Roman_Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times_New_Roman_Bold_Italic',
+  ),
+  'serif' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Times_New_Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times_New_Roman_Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times_New_Roman_Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times_New_Roman_Bold_Italic',
+  ),
+  'sans-serif' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Arial',
+    'bold' => DOMPDF_FONT_DIR . 'Arial_Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Arial_Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Arial_Bold_Italic',
+  ),
+  'helvetica' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Arial',
+    'bold' => DOMPDF_FONT_DIR . 'Arial_Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Arial_Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Arial_Bold_Italic',
+  ),
+  'zapfdingbats' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+    'bold' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+    'italic' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+    'bold_italic' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+  ),
+  'symbol' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Symbol',
+    'bold' => DOMPDF_FONT_DIR . 'Symbol',
+    'italic' => DOMPDF_FONT_DIR . 'Symbol',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Symbol',
+  ),
+  'fixed' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Courier_New',
+    'bold' => DOMPDF_FONT_DIR . 'Courier_New_Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Courier_New_Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Courier_New_Bold_Italic',
+  ),
+) ?>
